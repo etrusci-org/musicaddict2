24f114e122ba0581228561c9bd9dc69375214704
arT2 <60965610+etrusci-org@users.noreply.github.com>
2022-01-11 12:56:36 +0100

    Code doc.

--- dist/web/lib/api.php
+++ dist/web/lib/api.php
@@ -2,6 +2,9 @@
 require 'shared.php';
 
 
+/**
+ * MusicAddictAPI
+ */
 class MusicAddictAPI {
     protected $querySrc;
     protected $query;
@@ -9,7 +12,14 @@ class MusicAddictAPI {
     protected $Database;
 
 
+    /**
+     * Init class, on success parse request, and output.
+     *
+     * @param string $querySrc  From where to read the query. Can be post, get or both.
+     * @return void
+     */
     public function __construct($querySrc='post') {
+        // Decide where to read the query from.
         switch ($querySrc) {
             case 'post':
                 $this->querySrc = $querySrc;
@@ -59,12 +69,16 @@ class MusicAddictAPI {
         $this->output();
     }
 
-
-    // Parse and process query.
+    /**
+     * Parse and process query.
+     *
+     * @return void
+     */
     protected function parseQuery() {
         // Process query action
         switch ($this->query['action']) {
             case 'register':
+                // Open database for reading
                 $this->Database->open();
 
                 // Return new token but make sure it does not already exist.
@@ -80,64 +94,21 @@ class MusicAddictAPI {
                     }
                 }
 
+                // Close database.
                 $this->Database->close();
                 break;
 
             case 'continue':
+                // Stop if no valid token in query.
                 if (!isset($this->query['token']) || !array_key_exists('token', $this->query) || !isValidToken($this->query['token'])) {
                     $this->response['_errors'][] = 'missing query token.';
                     break;
                 }
 
+                // Open database for reading.
                 $this->Database->open();
 
-                $validCols = array_keys(array(
-                    'token'         => SQLITE3_TEXT,
-                    'firstPlayedOn' => SQLITE3_INTEGER,
-                    'playerName'    => SQLITE3_TEXT,
-                    'cash'          => SQLITE3_INTEGER,
-                    'records'       => SQLITE3_TEXT,
-                ));
-                $validCols = implode(', ', $validCols);
-
-                $q = sprintf('SELECT %s FROM sd WHERE token = :token;', $validCols);
-                $r = $this->Database->querySingle($q, array(
-                    array('token', ripemdHash($this->query['token']), SQLITE3_TEXT),
-                ));
-
-                $this->Database->close();
-
-                if (!$r) {
-                    $this->response['_errors'][] = 'Unknown token.';
-                    break;
-                }
-
-                $r['token'] = $this->query['token'];
-                $r['records'] = jdec($r['records']);
-
-                $this->response['saveData'] = $r;
-                break;
-
-            case 'save':
-                if (!isset($this->query['saveData']) || !array_key_exists('saveData', $this->query) || empty(trim($this->query['saveData']))) {
-                    $this->response['_errors'][] = 'missing query saveData.';
-                    break;
-                }
-
-                $saveData = jdec($this->query['saveData']);
-
-                if (!$saveData) {
-                    $this->response['_errors'][] = 'invalid query saveData.';
-                    break;
-                }
-
-                if (!isset($saveData['token']) || !array_key_exists('token', $saveData) || !isValidToken($saveData['token'])) {
-                    $this->response['_errors'][] = 'missing query token.';
-                    break;
-                }
-
-                $this->Database->open(TRUE);
-
+                // Define valid database table columns.
                 $validCols = array(
                     'token'         => SQLITE3_TEXT,
                     'firstPlayedOn' => SQLITE3_INTEGER,
@@ -145,21 +116,83 @@ class MusicAddictAPI {
                     'cash'          => SQLITE3_INTEGER,
                     'records'       => SQLITE3_TEXT,
                 );
-                // unset($saveData['playerName']); // sim error
 
+                // Query database for data.
+                $q = sprintf('SELECT %s FROM sd WHERE token = :token;', implode(', ', array_keys($validCols)));
+                $r = $this->Database->querySingle($q, array(
+                    array('token', ripemdHash($this->query['token']), SQLITE3_TEXT),
+                ));
+
+                // Close database.
+                $this->Database->close();
+
+                // Stop if no result.
+                if (!$r) {
+                    $this->response['_errors'][] = 'Unknown token.';
+                    break;
+                }
+
+                // Always re-set result token to what's in the database.
+                $r['token'] = $this->query['token'];
+
+                // Decode result records JSON.
+                $r['records'] = jdec($r['records']);
+
+                // Set response saveData to result.
+                $this->response['saveData'] = $r;
+                break;
+
+            case 'save':
+                // Stop if no saveData in query.
+                if (!isset($this->query['saveData']) || !array_key_exists('saveData', $this->query) || empty(trim($this->query['saveData']))) {
+                    $this->response['_errors'][] = 'missing query saveData.';
+                    break;
+                }
+
+                // Decode JSON saveData and stop on failure.
+                $saveData = jdec($this->query['saveData']);
+                if (!$saveData) {
+                    $this->response['_errors'][] = 'invalid query saveData.';
+                    break;
+                }
+
+                // Check for valid token in saveData.
+                if (!isset($saveData['token']) || !array_key_exists('token', $saveData) || !isValidToken($saveData['token'])) {
+                    $this->response['_errors'][] = 'missing query token.';
+                    break;
+                }
+
+                // Open database for reading and writing.
+                $this->Database->open(TRUE);
+
+                // Define valid database table columns.
+                $validCols = array(
+                    'token'         => SQLITE3_TEXT,
+                    'firstPlayedOn' => SQLITE3_INTEGER,
+                    'playerName'    => SQLITE3_TEXT,
+                    'cash'          => SQLITE3_INTEGER,
+                    'records'       => SQLITE3_TEXT,
+                );
+                // unset($saveData['playerName']); // simulate error
+
+                // Make sure we got all the data we need.
                 $colDiff = array_diff_key($validCols, $saveData);
                 if ($colDiff) {
                     $this->response['_errors'][] = 'Missing saveData keys: '.implode(', ', array_keys($colDiff));
                     return;
                 }
 
+                // Sanitize playerName and set to default if empty.
                 $saveData['playerName'] = substr(trim($saveData['playerName']), 0, 30);
                 $saveData['playerName'] = preg_replace('/[^A-Za-z0-9_-]/', '', $saveData['playerName']);
                 if (empty($saveData['playerName'])) {
                     $saveData['playerName'] = 'Anonymous';
                 }
+
+                // Encode records to JSON.
                 $saveData['records'] = jenc($saveData['records']);
 
+                // Create initial row if it does not exist yet.
                 $q = 'SELECT token FROM sd WHERE token = :token;';
                 $v = array(
                     array('token', ripemdHash($saveData['token']), SQLITE3_TEXT),
@@ -173,6 +206,7 @@ class MusicAddictAPI {
                     }
                 }
 
+                // Update database data.
                 $c = array(
                     'lastSavedOn = :lastSavedOn',
                 );
@@ -191,13 +225,16 @@ class MusicAddictAPI {
                 $c = implode(', ', $c);
                 $q = sprintf('UPDATE sd SET %s WHERE token = :token;', $c);
 
+                // Stop on update error.
                 if (!$this->Database->write($q, $v)) {
                     $this->response['_errors'][] = 'Could not save to database.';
                     return;
                 }
 
+                // Close database.
                 $this->Database->close();
 
+                // All good if we reach this line.
                 $this->response['saved'] = TRUE;
                 break;
 
@@ -206,8 +243,11 @@ class MusicAddictAPI {
         }
     }
 
-
-    // Output response and exit.
+    /**
+     * Output response as JSON and exit.
+     *
+     * @return void
+     */
     protected function output() {
         header('Content-type: application/json; charset=utf-8');
         print(jenc($this->response));
